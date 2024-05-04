3978f3063bc188bff9ab2ff9b0a6d55dcddbcec0
Kasemsun <Kasemsun4869@gmail.com>
2024-05-04 14:11:43 +0700

    initial

---
+++ src/main/java/com/example/javaDeveloper/JavaDeveloperApplication.java
@@ -0,0 +1,17 @@
+package com.example.javaDeveloper;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@SpringBootApplication
+public class JavaDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDeveloperApplication.class, args);
+	}
+
+}
